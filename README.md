ec47d3461e142d6ddf2febf9de940179d09bf707
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2018-04-01 18:43:41 +0200

    Going git

---
+++ README.md
@@ -0,0 +1,34 @@
+# Simple Chat Server
+
+Simple socket.io based chat server that can be used for various workshops.
+
+## Getting started
+
+Clone the repo and install dependencies:
+```
+git clone git@github.com:zbicin/simple-chat-server.git
+cd simple-chat-server
+npm install
+```
+
+...and run the server:
+```
+npm start
+```
+
+By default the app runs on port 3000. If you wish to change it, use either the `--port` argument: 
+
+```
+npm start -- --port 1234
+```
+
+or `SIMPLE_CHAT_PORT` environmental variable:
+
+```
+export SIMPLE_CHAT_SERVER_PORT=1234
+npm start
+```
+
+## Client development
+
+Use [simple-chat-client-core](https://github.com/zbicin/simple-chat-client-core).
---
+++ index.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var readme = fs.readFileSync(path.join(__dirname, 'README.md'));
+var port = getPort();
+
+var messages = [];
+var users = {};
+
+io.origins('*:*');
+
+app.get('/', function (req, res) {
+    res.type('text/plain');
+    res.send(readme);
+    res.end();
+});
+
+io.on('connection', function (socket) {
+    console.log('socket ' + socket.id + ' connected');
+    socket.emit('user list', Object.values(users));
+    socket.emit('chat history', messages);
+
+    socket.on('user join', function (name) {
+        console.log('socket ' + socket.id + ' is called "' + name + '"');
+        users[socket.id] = name;
+        io.emit('user join', name);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('user "' + users[socket.id] + '" disconnected');
+        io.emit('user left', users[socket.id]);
+        delete users[socket.id];
+    });
+
+    socket.on('chat message', function (msg) {
+        msg.author = users[socket.id];
+        io.emit('chat message', msg);
+        messages.push(msg);
+    });
+});
+
+http.listen(port, function () {
+    console.log('Server running on *:' + port);
+});
+
+function getPort() {
+    var argumentIndex = process.argv.indexOf('--port');
+    if (argumentIndex > -1) {
+        return process.argv[argumentIndex + 1];
+    }
+
+    return process.env.SIMPLE_CHAT_SERVER_PORT || 3000;
+}
